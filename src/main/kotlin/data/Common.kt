c8f1565b79f79a16aab37fd607276dc9e104fe08
Ved <dev@vedat.xyz>
2022-06-19 19:39:47 +0300

    Add factory method to `Semester`

--- src/main/kotlin/data/Common.kt
+++ src/main/kotlin/data/Common.kt
@@ -1,6 +1,8 @@
 package data
 
 import kotlinx.serialization.Serializable
+import org.w3c.dom.Text
+import java.util.regex.Pattern
 
 @Serializable
 data class Course(val department: String, val number: String)
@@ -12,7 +14,16 @@ data class CourseSection(val department: String, val number: String, val section
 enum class SemesterType(val value: Int) { Fall(1), Spring(2), Summer(3) }
 
 @Serializable
-data class Semester(val year: String, val season: SemesterType)
+data class Semester(val year: String, val season: SemesterType) {
+    companion object {
+        private val semesterPattern = Pattern.compile("(\\d{4})-\\d{4}\\W(Fall|Spring|Summer)")
+
+        @JvmStatic
+        fun from(text: String): Semester? = semesterPattern.matcher(text.trim()).run {
+            if (find()) Semester(group(1), SemesterType.valueOf(group(2))) else null
+        }
+    }
+}
 
 enum class LetterGrade(val representation: String) {
     A("A"),
@@ -30,6 +41,7 @@ enum class LetterGrade(val representation: String) {
 
     companion object {
         private val map = values().associateBy(LetterGrade::representation)
+
         @JvmStatic
         fun from(representation: String) = map[representation] ?: Unknown
     }
