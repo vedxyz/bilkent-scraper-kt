4764a91c653df19a05a26e56a4fc74d9226e089d
Ved <dev@vedat.xyz>
2022-06-19 05:31:00 +0300

    Implement restricted mode functions

---
+++ src/main/kotlin/data/RestrictedMode.kt
@@ -0,0 +1,34 @@
+package data
+
+import OkHttpSingleton
+import kotlinx.serialization.Serializable
+import okhttp3.FormBody
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+
+@Serializable
+data class LetterGradeResult(val course: String, val grade: LetterGrade)
+
+internal fun parseSRSRestriction(dom: Document) =
+    dom.selectFirst("#wrapper > div.info")?.text()?.contains("SRS is in restricted mode") ?: false
+
+internal fun parseLetterGrades(dom: Document) = dom.select("#wrapper > fieldset > table > tbody > tr").map { row ->
+    LetterGradeResult(row.child(0).text().trim(), row.child(1).text().let { LetterGrade.from(it) })
+}
+
+internal suspend fun isSRSRestricted(): Boolean {
+    OkHttpSingleton.fetch("https://stars.bilkent.edu.tr/srs/").use {
+        return parseSRSRestriction(Jsoup.parse(it.body!!.string()))
+    }
+}
+
+internal suspend fun getLetterGrades(id: String, password: String): List<LetterGradeResult> {
+    OkHttpSingleton.fetch(
+        "https://stars.bilkent.edu.tr/srs/index.php",
+        HttpMethods.GET,
+        null,
+        FormBody.Builder().add("user_id", id).add("password", password).build()
+    ).use {
+        return parseLetterGrades(Jsoup.parse(it.body!!.string()))
+    }
+}
