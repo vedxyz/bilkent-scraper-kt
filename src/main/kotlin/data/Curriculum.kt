09e20a873c4c63071a6c36b48b0450ffaadd7660
Ved <dev@vedat.xyz>
2022-06-19 19:41:25 +0300

    Refactor `Curriculum` to use the factory method for semester

--- src/main/kotlin/data/Curriculum.kt
+++ src/main/kotlin/data/Curriculum.kt
@@ -23,33 +23,26 @@ data class CurriculumCourseItem(
 @Serializable
 data class CurriculumSemester(val items: List<CurriculumCourseItem>)
 
-private val semesterPattern = Pattern.compile("(\\d{4})-\\d{4}\\W(Fall|Spring|Summer)")
 private val replacementPattern = Pattern.compile("([A-Z]+)\\W(\\d+)\\W(.*)\\W?")
 
 internal fun parseCurriculum(dom: Document) = dom.select("table.printMod").drop(2).dropLast(2).map { table ->
     table.select("tbody > tr").drop(1).map { row ->
         val course = row.child(0).text().trim().split(" ")
 
-        val semesterMatcher = row.child(5).text().trim().let { semesterPattern.matcher(it) }
-        val replacementMatcher = row.child(6).text().trim().let { replacementPattern.matcher(it) }
-
         CurriculumCourseItem(course = if (course.size != 2) null else Course(course[0], course[1]),
             name = row.child(1).text().trim().ifEmpty { "N/A" },
             status = row.child(2).text().trim(),
             grade = row.child(3).text().trim().let { LetterGrade.from(it.ifEmpty { "Unknown" }) },
             credits = row.child(4).text().trim().ifEmpty { "N/A" },
-            semester = if (semesterMatcher.find()) semesterMatcher.run {
-                Semester(
-                    year = group(1), season = SemesterType.valueOf(group(2))
-                )
-            } else null,
-            replacement = if (replacementMatcher.find()) replacementMatcher.run {
-                CurriculumCourseReplacement(
+            semester = row.child(5).text().let { Semester.from(it) },
+            replacement = row.child(6).text().trim().let { replacementPattern.matcher(it) }.run {
+                if (find()) CurriculumCourseReplacement(
                     Course(
                         department = group(1), number = group(2)
                     ), name = group(3)
                 )
-            } else null)
+                else null
+            })
     }.let { CurriculumSemester(it) }
 }
 
