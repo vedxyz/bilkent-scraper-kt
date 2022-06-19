3e1acc568d4b72a4fba490601e0544abf16e1c3f
Ved <dev@vedat.xyz>
2022-06-19 05:28:37 +0300

    Add Serializable tag to common data classes

--- src/main/kotlin/data/Common.kt
+++ src/main/kotlin/data/Common.kt
@@ -1,9 +1,17 @@
 package data
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Course(val department: String, val number: String)
+
+@Serializable
 data class CourseSection(val department: String, val number: String, val section: String)
 
+@Serializable
 enum class SemesterType(val value: Int) { Fall(1), Spring(2), Summer(3) }
+
+@Serializable
 data class Semester(val year: String, val season: SemesterType)
 
 enum class LetterGrade(val representation: String) {
